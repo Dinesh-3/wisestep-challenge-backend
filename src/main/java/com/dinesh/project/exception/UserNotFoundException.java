912ba54b555b4ae6b4a6eddfb256a0ea1eb62d74
dinesh <dinesh@codingmart.com>
2022-05-22 12:52:27 +0530

    validation change

--- src/main/java/com/dinesh/project/exception/UserNotFoundException.java
+++ src/main/java/com/dinesh/project/exception/UserNotFoundException.java
@@ -3,7 +3,7 @@ package com.dinesh.project.exception;
 public class UserNotFoundException extends RuntimeException{
 
     public UserNotFoundException() {
-        super("User Not Found");
+        super("User is Not Found");
     }
 
     public UserNotFoundException(String message) {
